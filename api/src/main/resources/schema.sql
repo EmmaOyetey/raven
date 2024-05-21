b1d04ff37f12de498d708973a66a7eb058bbfc5b
Emma Oyetey <emmaflookis@hotmail.com>
2024-05-21 10:32:28 +0100

    database raven, table "organisation" successfully created with 2 test organisations

--- api/src/main/resources/schema.sql
+++ api/src/main/resources/schema.sql
@@ -6,22 +6,18 @@ USE raven;
 
 CREATE TABLE `organisation` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `organisation_name` varchar(1000) DEFAULT NULL,
-  `organisation_logo` varchar(255) NOT NULL,
-  `organisation_overview` varchar(1000) DEFAULT NULL,
-  `organisation_link` varchar(1000) DEFAULT NULL,
+  `name` varchar(1000) DEFAULT NULL,
+  `logo` varchar(255) DEFAULT NULL,
+  `overview` varchar(1000) DEFAULT NULL,
+  `link` varchar(1000) DEFAULT NULL,
   `impact` varchar(1000) DEFAULT NULL,
   `careers` BOOLEAN DEFAULT FALSE,
   `creative_arts` BOOLEAN DEFAULT FALSE,
   `curriculum`BOOLEAN DEFAULT FALSE,
   `life_skills`BOOLEAN DEFAULT FALSE,
   `mental_health` BOOLEAN DEFAULT FALSE,
-  `physical_activity`BOOLEAN DEFAULT FALSE,
+  `physical_activity` BOOLEAN DEFAULT FALSE,
   `pshe`BOOLEAN DEFAULT FALSE,
   `tech` BOOLEAN DEFAULT FALSE,
-
    PRIMARY KEY (`id`)
 );
-
-<--`delivery_method` ENUM('in-person', 'remote', 'teacher-training') DEFAULT NULL,-->
-<--`age_group` ENUM('KS1', 'KS2, 'KS3', 'Teacher') DEFAULT NULL

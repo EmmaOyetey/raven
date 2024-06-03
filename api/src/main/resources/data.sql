6f6f1ba5e3bf410fee76646a93a0ea4c02a5c4a5
Emma Oyetey <emmaflookis@hotmail.com>
2024-06-03 16:15:45 +0100

    added more organisations and controller and service updated to enable form data input to be saved in raven database

--- api/src/main/resources/data.sql
+++ api/src/main/resources/data.sql
@@ -61,3 +61,95 @@ FALSE,
 TRUE,
 FALSE
 );
+
+INSERT INTO organisation (
+`name`,
+`logo`,
+`overview`,
+`link`,
+`impact`,
+`careers`,
+`creative_arts`,
+`curriculum`,
+`life_skills`,
+`mental_health`,
+`physical_activity`,
+`pshe`,
+`tech`
+)
+VALUES (
+"Career Ready",
+"https://th.bing.com/th?id=OIP.ED1TdaMGR5mq2oETmti0NgHaHN&w=253&h=246&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+"We believe that every young person should have the opportunity to kickstart a rewarding future. Yet, due to the social mobility challenge in the UK, the futures of far too many young people are determined by background, not potential.We exist to fix this by connecting young people to the world of work",
+"https://careerready.org.uk/",
+"https://careerready.org.uk/annual-report-and-impact/,
+TRUE,
+FALSE,
+FALSE,
+FALSE,
+FALSE,
+FALSE,
+FALSE,
+FALSE
+);
+INSERT INTO organisation (
+`name`,
+`logo`,
+`overview`,
+`link`,
+`impact`,
+`careers`,
+`creative_arts`,
+`curriculum`,
+`life_skills`,
+`mental_health`,
+`physical_activity`,
+`pshe`,
+`tech`
+)
+VALUES (
+"Speakers for Schools",
+"https://th.bing.com/th/id/OIP.3BHLyhzYG9a5mAbsuCcWXgHaHa?w=172&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+"We aim to close the opportunity gap and level the playing field for all 11-19-year-olds from state schools and colleges by providing fair access to fully funded work experience and inspirational talks that will help them to reach their full career potential.",
+"https://www.speakersforschools.org/",
+"https://www.speakersforschools.org/about/",
+TRUE,
+FALSE,
+FALSE,
+FALSE,
+FALSE,
+FALSE,
+FALSE,
+FALSE
+);
+INSERT INTO organisation (
+`name`,
+`logo`,
+`overview`,
+`link`,
+`impact`,
+`careers`,
+`creative_arts`,
+`curriculum`,
+`life_skills`,
+`mental_health`,
+`physical_activity`,
+`pshe`,
+`tech`
+)
+VALUES (
+"The Brilliant Club",
+"https://th.bing.com/th?id=OIP.KE_iATpKZLQNV8mkAQJq9gAAAA&w=249&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+"We are an award-winning university access charity
+"The Brilliant Club works with schools and universities across the UK. We mobilise the PhD community to support students who are less advantaged to access the most competitive universities and succeed when they get there. ",
+"https://thebrilliantclub.org/",
+"https://thebrilliantclub.org/evaluation/programmes-evaluation/",
+FALSE,
+FALSE,
+TRUE,
+FALSE,
+FALSE,
+FALSE,
+FALSE,
+FALSE
+);

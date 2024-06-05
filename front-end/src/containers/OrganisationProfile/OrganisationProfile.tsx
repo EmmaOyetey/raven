cd7816625196dd1e13a39ab40cf7ef95c3944a7a
Emma Oyetey <emmaflookis@hotmail.com>
2024-06-05 20:25:53 +0100

    styling to organisationInfo

--- front-end/src/containers/OrganisationProfile/OrganisationProfile.tsx
+++ front-end/src/containers/OrganisationProfile/OrganisationProfile.tsx
@@ -2,6 +2,7 @@
 import { useParams } from 'react-router-dom';
 import OrganisationInfo from '../../components/OrganisationInfo/OrganisationInfo';
 import OrganisationType from '../../types/organisationType';
+import Nav from '../../components/Nav/Nav';
 
 type OrganisationProfileProps = {
   allOrganisations: OrganisationType[];
@@ -12,10 +13,19 @@ const OrganisationProfile = ({ allOrganisations }: OrganisationProfileProps) => 
   const organisation = allOrganisations.find(org => org.name === organisationName);
 
   if (!organisation) {
-    return <div>Organisation not found</div>;
+    return (
+      <div>
+        <Nav /> {/* Add the Nav component here */}
+        Organisation not found
+      </div>
+    );
   }
 
-  return <OrganisationInfo organisation={organisation} />;
+  return (
+    <div>
+      <Nav /> {/* Add the Nav component here */}
+      <OrganisationInfo organisation={organisation} />
+    </div>
+  );
 };
-
 export default OrganisationProfile;

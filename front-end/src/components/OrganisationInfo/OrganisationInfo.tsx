799cedf8379f2a7c8335bcbdbb0cd70caa446398
Emma Oyetey <emmaflookis@hotmail.com>
2024-06-06 09:49:41 +0100

    added styling to organisationInfo component

--- front-end/src/components/OrganisationInfo/OrganisationInfo.tsx
+++ front-end/src/components/OrganisationInfo/OrganisationInfo.tsx
@@ -17,40 +17,47 @@ const OrganisationInfo = ({ organisation }: OrganisationInfoProps) => {
   }, [organisationName]);
 
   const supportAreas = Object.keys(organisation)
-  .filter((key) => organisation[key as keyof OrganisationType] === true)
-  .map((key) => key.replace(/([A-Z])/g, ' $1').trim())
-  .map((key) =>
-    key
-      .split(' ')
-      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(' ')
-  );
+    .filter((key) => organisation[key as keyof OrganisationType] === true)
+    .map((key) => key.replace(/([A-Z])/g, ' $1').trim())
+    .map((key) =>
+      key
+        .split(' ')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ')
+    );
 
   return (
-    <article className="organisation-info">
-      <div className="organisation-info__banner">
-        <img className="organisation-info__image" src={organisation.logo ?? undefined} alt={organisation.name} />
-      </div>
-      <div className="organisation-info__content">
-        <div className="organisation-info__header">
-          <h2 className="organisation-info__name">
-            {organisation.name}
-          </h2>
-          <Link to="/organisations" className="close-link">
-            <CloseIcon /> {/* Use the CloseIcon component directly */}
+    <>
+      <article className="organisation-info">
+        <div className="organisation-info__banner">
+          <Link to="/organisations" className="organisation-info__close-link">
+            <CloseIcon />
           </Link>
         </div>
-        <div className="organisation-info__supportAreas">
-          <h3 className="organisation-info__supportAreas-header">Support Areas:</h3>
-          <p>
-            {supportAreas.join(", ")}
+        <div className="organisation-info__content">
+          <div className="organisation-info__header">
+            <img className="organisation-info__image" src={organisation.logo ?? undefined} alt={organisation.name} />
+            <h2 className="organisation-info__name">{organisation.name}</h2>
+          </div>
+          <div className="organisation-info__support-areas">
+            <h3 className="organisation-info__support-areas-header">Support Areas:</h3>
+            <p>
+              {supportAreas.map((area, index) => (
+                <span key={index}>{area}{index < supportAreas.length - 1 ? ", " : ""}</span>
+              ))}
+            </p>
+          </div>
+          <h3 className="organisation-info__overview-header">About:</h3>
+          <p className="organisation-info__overview">{organisation.overview ?? `No description available`}</p>
+          <p className="organisation-info__website-url">
+            For more info visit: <a href={organisation.websiteURL} target="_blank" rel="noopener noreferrer">{organisation.websiteURL}</a>
           </p>
-        </div> 
-        <p className="organisation-info__overview">About: {organisation.overview ?? `No description available`}</p>
-        <p className="organisation-info__websiteURL">For more info visit: {organisation.websiteURL}</p>
-        <p className="organisation-info__impactURL">Find out about their impact: {organisation.impactURL}</p>
-      </div>
-    </article>
+          <p className="organisation-info__impact-url">
+            Find out about their impact: <a href={organisation.impactURL} target="_blank" rel="noopener noreferrer">{organisation.impactURL}</a>
+          </p>
+        </div>
+      </article>
+    </>
   );
 };
 
